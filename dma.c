12297cbb6183cd90f395575b30acc8f26191c04c
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2021-08-08 17:17:10 +0545

    Changes made

--- dma.c
+++ dma.c
@@ -7,23 +7,26 @@ typedef struct{
     int batch;
 }details;
 int main(){
-    int n;
+    // char response;
+    int i,n;
     printf("Enter the number of students:");
-    scanf("%d",n);
+    scanf("%d",&n);
     details *student;
-    student = (details *) malloc(n*sizeof(details));
+    student = (details *) malloc(n*sizeof(details)); //or calloc(n,sizeof(details))
     for(int i=0;i<n;i++){
+        printf("\n\n");
         printf("Enter the name of %dth student:",i+1);
         scanf("%s",student[i].name);
         printf("Enter the age of %dth student:",i+1);
-        scanf("%d",student[i].age);
+        scanf("%d",&student[i].age);
         printf("Enter the 4 digit student id of %dth student:",i+1);
-        scanf("%d",student[i].id);
+        scanf("%d",&student[i].id);
         printf("Enter the batch-year of %dth student:",i+1);
-        scanf("%d",student[i].batch);
+        scanf("%d",&student[i].batch);
     }
-    printf("Name:%s\n",*student[0].name);
-
+    printf("\nEnter which student you want details of:\n\n");
+    scanf("%d",&i);
+    printf("\n\nName:%s\nAge:%d\nID:%d\nBatch:%d",(student+i-1)->name,(student+i-1)->age,(student+i-1)->id,(student+i-1)->batch);
 
     return 0;
 }
---
+++ pointers3.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<GL/gl.h>
+int main(){
+    glBegin(GL_POLYGON);
+    // glUseProgram();
+    glEnd();
+    return 0;
+}
